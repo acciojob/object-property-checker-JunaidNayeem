aa40baf4a0cf77adac8c30664d697c7a0fddba46
JunaidNayeem <78254107+JunaidNayeem@users.noreply.github.com>
2023-07-08 23:50:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
   //   write your code here
-	for (let object for sampleObject) {
+	for (let object in sampleObject) {
 		if(key===object)
 		{
 			return true;
